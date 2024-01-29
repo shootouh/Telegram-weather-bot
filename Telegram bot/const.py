ff24f76f5b0ec438d92a24ebc3fad7dc67bcb9e1
shootouh <143724919+shootouh@users.noreply.github.com>
2024-01-29 17:42:11 +0100

    Update const.py

--- Telegram bot/const.py
+++ Telegram bot/const.py
@@ -1,11 +1,11 @@
-TOKEN = '6067418029:AAFtNW-ZGd6xq22l9-qp617oO9Wi4R2Gj9s'
+TOKEN = ''
 
 URL = 'https://api.telegram.org/bot{token}/{method}'
 
 UPDATE_METH = 'getUpdates'
 SEND_METH = 'sendMessage'
 
-MY_ID = 6692595913
+MY_ID = 
 
 UPDATE_ID_FILE_PATH = 'update_id' 
 
@@ -17,6 +17,6 @@ except FileNotFoundError:
 except ValueError:
     UPDATE_ID = None  # File does not contain a valid integer.
 
-WEATHER_TOKEN = '4b320ea9dc811c5d7c970fbfa00f3fb2'
+WEATHER_TOKEN = ''
 
 WEATHER_URL = 'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={token}'
